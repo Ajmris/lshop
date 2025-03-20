ace16825b4f8ef2d5a47aae466f5007d8a905c7a
ajmris <szymektarasiewicz@onet.pl>
2025-03-21 00:44:52 +0100

    add Auth, bootstrap and table

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/users/list', [UserController::class, 'index'])->middleware('auth');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
